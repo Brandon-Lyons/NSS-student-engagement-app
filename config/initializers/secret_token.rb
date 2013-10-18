b0669a71d2551ce5e3b7733a9ca9b54b957716b3
Brandon-Lyons <brandon.joseph.lyons@gmail.com>
2013-10-18 11:30:40 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NSSStudentEngagementApp::Application.config.secret_key_base = '0e12a68b027253a4af97a945025b323330396f360af4b80aaf21bd99a2c0c6e09000fe9e066fcd56552c5805a012820e0157c9255e348512a50938ecf9c84af6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NSSStudentEngagementApp::Application.config.session_store :cookie_store, key: '_NSS_student_engagement_app_session'
