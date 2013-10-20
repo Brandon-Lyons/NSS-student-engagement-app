d06df5f5df613816e9f8ba0e93e3d71595fe2449
desmondcain <desmondcain@gmail.com>
2013-10-20 10:26:15 -0500

    add secret token environment variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,5 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-NSSStudentEngagementApp::Application.config.secret_token = 'dd2851f89c2ac961fd128a522776f95b6d508addbf766d196180ba80bc8620590652778b205815c8e7ac3d1cf933eadfa4776bbd19705341b07eb2cfc40e249c'
+if Rails.env.development? or Rails.env.test?
+	NSSStudentEngagementApp::Application.config.secret_token = 'dd2851f89c2ac961fd128a522776f95b6d508addbf766d196180ba80bc8620590652778b205815c8e7ac3d1cf933eadfa4776bbd19705341b07eb2cfc40e249c'
+else
+	NSSStudentEngagementApp::Application.config.secret_token = ENV['SECRET_TOKEN']
+end
